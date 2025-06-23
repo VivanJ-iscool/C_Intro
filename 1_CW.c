12a4a8d33cd9b1a45e0f8e4d269e99a730a552c8
VivanJ-iscool <vivanrocksio@gmail.com>
2025-06-23 18:57:22 -0400

    Update 1_CW.c

--- 1_CW.c
+++ 1_CW.c
@@ -1,5 +1,25 @@
-#include <stdio.h>
+// #include <stdio.h>
 
+// int main() {
+
+//     printf("Hello World!\nMy name is Vivan and I... I will be the king of the pirates! \n");
+// }
+//This is a comment
+
+/*This type of comment has a beggining and end, incase you wanna sandwich it between lines of code!*/
+
+
+// #include <stdio.h>
+// int main() {
+//     printf("Chocolate chip cookies are way better than oatmeal raisin cookies\n");
+//     printf("Learning the C programming language is an exciting adventure!\n"); /*In this string, I feel like c is fun because it is a combonation of everything I've learned so far!*/
+   
+//     //The statement about cokkies is printed and using \n, it adds a newline for the next statement about c!
+// }
+
+
+#include <stdio.h>
 int main() {
-    printf("Hello World!\n");
+    //output a line
+    printf("Hello World! My name is Vivan!\n");
 }
