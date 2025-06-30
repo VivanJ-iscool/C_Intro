d47e56a8d3b061b9c789b43dc4b0e23314279f39
VivanJ-iscool <vivanrocksio@gmail.com>
2025-06-30 18:51:51 -0400

    Add files via upload

---
+++ 2_CW.c
@@ -0,0 +1,13 @@
+#include <stdio.h>
+int main(){
+
+    int number/*setting up integer variable*/ = 10/*giving value*/;
+    printf("yap yapity yap\n%d",number);
+}
+
+
+//4 main types of variables- integers, floats, doubles and characters
+
+//double- numbers w/ more than 8 decimal places
+
+//char- strings basically
