d5d3d5de7ed8a29c92db6faf6e03af36e89e7c4a
VivanJ-iscool <vivanrocksio@gmail.com>
2025-09-29 19:01:24 -0400

    Update 5_CW.c

--- 5_CW.c
+++ 5_CW.c
@@ -1,10 +1,9 @@
 
 
-//3 MAIN TYPES OF LOOPS: While Loops, 
-//Do-While Loops & For Loops
+// 3 MAIN TYPES OF LOOPS: While Loops,
+// Do-While Loops & For Loops
 
-
-//Interacting with a loop (First Intro Example)
+// Interacting with a loop (First Intro Example)
 
 // #include <stdio.h>
 // int main(){
@@ -16,7 +15,7 @@
 //     scanf("%d", pin);
 
 //     tries++;
-    
+
 //     while (pin != 1234 && tries < 3) {
 //         printf("Enter your PIN: ");
 //         scanf("%d", &pin); //scanf is considered as an input; basically it allows you to enter something and scans your entered input
@@ -30,45 +29,87 @@
 
 // }
 
+// Guess the number (while loop interaction)
+
+// #include <stdio.h>
+// int main()
+// {
+//     int guess;
+//     int tries = 0;
+
+//     printf("I’m thinking of a number in the range 1-10.\n");
+//     printf("Try to guess it: ");
+
+//     scanf("%d", &guess);
+
+//     // Write a while loop here:
+//     while (guess != 8)
+//     {
+//         printf("I’m thinking of a number in the range 1-10.\n");
+//         printf("Try to guess it: ");
+//         scanf("%d", &guess);
+//     }
+
+//     if (guess == 8)
+//     {
+//         printf("You got it!\n");
+//     }
+// }
+
+// Making a while loop by myself
+
+// #include <stdio.h>
+// int main()
+// {
+//     int i = 0;
+
+//     // Write a while loop here:
+//     while (i != 9){
+//         int square = i*i;
+//         printf("%d\t%d \n", i, square);
+//         i++;
+//     }
+// }   
+
+//oposite (squares from 9-0)
+// #include <stdio.h>
+// int main()
+// {
+//     int i = 9;
+
+//     // Write a while loop here:
+//     while (i != 0){
+//         int square = i*i;
+//         printf("%d\t%d \n", i, square);
+//         i--;
+//     }
+// }   
 
 
-//Guess the number (while loop interaction)
+//Do-While Loop
 
 
-#include <stdio.h>
-int main() {
-    int guess;
-    int tries = 0;
-        
-    printf("I’m thinking of a number in the range 1-10.\n");
-    printf("Try to guess it: ");
-        
-    scanf("%d", &guess);
-        
-    // Write a while loop here:
-    while (guess != 8){
-        printf("I’m thinking of a number in the range 1-10.\n");
-        printf("Try to guess it: ");
-        scanf("%d", &guess); 
-    }
+// #include <stdio.h>
+// int main() {
+//     int i = 9;
     
-    if (guess == 8) {
-        printf("You got it!\n");
-    }
-}
+//     do{
+//         printf("%d\n", i);
+//         i++;
+//     } while (i < 10); 
+        
+// }
 
-
-//Making a while loop by myself
+//For Loop
 
 #include <stdio.h>
-int main() {
-    int i = 0;
-    int square = sqrt(i);
-
-    // Write a while loop here:
-    while (i != 9)
-        printf("%d\t%d")
-
-
+    int main() {
+        int i = 99;
+        
+        // Write your loop here
+        while (i!=0){
+            printf("%d bottles of pop on the wall!\nTake one down and pass it around\n%d bottles of pop on the wall", i);
+            i--;
+    }
 
 }
