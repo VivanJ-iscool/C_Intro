00896254e4828105abfcf0cbdaa5355b4e5999f5
VivanJ-iscool <vivanrocksio@gmail.com>
2025-10-13 20:57:44 -0400

    Update 5_CW.c

--- 5_CW.c
+++ 5_CW.c
@@ -190,3 +190,31 @@
 
 //10
 //c
+
+//Project- Fizz Buzz
+#include <stdio.h>
+int main(){
+    for (int i = 1; i<=100; i++){
+        
+        if (i%15 == 0){
+            printf("FizzBuzz\n");
+        }
+        
+        else if (i%3 == 0){
+            printf("Fizz\n");
+        }
+
+        else if (i%5 == 0){
+            printf("Buzz\n");
+        }
+
+
+
+        else {
+            printf("%d\n",i);
+        }
+
+    }
+
+
+}
