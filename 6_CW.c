6c41f7d67e91eaa515df31bb3f7b67286e08b555
VivanJ-iscool <vivanrocksio@gmail.com>
2025-10-27 19:00:18 -0400

    Update 6_CW.c

--- 6_CW.c
+++ 6_CW.c
@@ -1,5 +1,45 @@
 //6.2 Strings
 
+//6.1.2 Creating and Initializing Strings
 
-//1st way of initializing strings
+//1st & 2nd way of initializing strings
+// #include <stdio.h>
+// int main(){
+//     char greet[] = "Hello World";
+//     char greet2[]={'H', 'E', 'L', 'L', 'O', ' ', 'W', 'O', 'R', 'L', 'D', '\0'};
+//     printf("%s\n", greet);
+//     printf("%s", greet2);
+// }
+
+// #include<stdio.h>
+// int main() {
+//     // Checkpoint 1 code goes here
+//     char comp[] = {'C', 'O', 'M', 'P', 'U', 'T', 'E', 'R', '\0'};
+//     // Checkpoint 2 code goes here
+//     char compCode[] = "Computer Code";
+//     // Checkpoint 3 code goes here
+//     printf("%s\n%s\n", comp, compCode);
+// }
+
+
+//6.1.3 Character Access and Modification
+
+// #include<stdio.h>
+// int main() {
+//     char srt[] = "Yo_Wsp_Guys";
+//     printf("%c\n", srt[6]);
+// }
+
+
+// #include <stdio.h>
+// int main() {
+//     char p[] = "procezzor";
+//     // Checkpoint 1 code goes here
+//     printf("%c\n", p[3]);
+//     // Checkpoint 2 code goes here
+//     printf("%s", p);
+// }
+
+//6.1.4 Looping Through Stringsg strings
 char str[]
+
